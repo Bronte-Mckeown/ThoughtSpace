d92507cf707ab113ad4835d8c5cb577440c57c24
Ian <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2023-03-21 12:35:28 -0400

    fixed pathing

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from ThoughtSpace._base import basePCA
 
-data = pd.read_csv("output.csv")
+data = pd.read_csv("scratch//data//output.csv")
 model = basePCA()
 output = model.fit_project(data)
 loadings = model.loadings
