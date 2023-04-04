1b0bac8720e23b252634c04d316fc1228cd08139
Ian-Goodall-Halliwell <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2023-04-04 12:16:49 -0400

    added env

--- example.py
+++ example.py
@@ -4,5 +4,6 @@ from ThoughtSpace._base import basePCA
 data = pd.read_csv("scratch//data//output.csv")
 model = basePCA()
 output = model.fit_project(data)
-#loadings = model.loadings
 model.save(path="results",pathprefix="PCA_results")
+
+
