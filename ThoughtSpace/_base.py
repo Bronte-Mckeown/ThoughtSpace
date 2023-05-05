61da6319d114f2a1c29d9dee504871b8edbf209e
Ian-Goodall-Halliwell <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2023-05-05 15:58:16 -0400

    Fixing single file output

--- ThoughtSpace/_base.py
+++ ThoughtSpace/_base.py
@@ -158,6 +158,9 @@ class basePCA(TransformerMixin, BaseEstimator):
         if scale:
             _df = self.z_score(_df)
         self.loadings = self.naive_pca(_df)
+        indivloadings = np.dot(_df,self.loadings).T
+        for x in range(self.n_components):
+            self.extra_columns[f"PCA_{x}"] = indivloadings[x, :]
         return self
 
     def transform(
@@ -195,9 +198,10 @@ class basePCA(TransformerMixin, BaseEstimator):
             os.makedirs(os.path.join(self.path,"screeplots"),exist_ok=True)
             
             save_wordclouds(self.loadings,os.path.join(self.path,"wordclouds"))
-            self.project_columns.to_csv(os.path.join(self.path,"csvdata","pca_scores.csv"))
+            self.project_columns.to_csv(os.path.join(self.path,"csvdata","projected_pca_scores.csv"))
+            self.extra_columns.to_csv(os.path.join(self.path, "csvdata","fitted_pca_scores.csv"))
             self.loadings.to_csv(os.path.join(self.path,"csvdata","pca_loadings.csv"))
-            #pd.concat([self.ogdf, self.check_inputs(self.project_columns)], axis=1).to_csv(os.path.join(self.path,"csvdata","pca_scores_original_format.csv"))
+            pd.concat([self.ogdf, self.check_inputs(self.extra_columns)], axis=1).to_csv(os.path.join(self.path,"csvdata","pca_scores_original_format.csv"))
             plot_scree(self.fullpca,os.path.join(self.path, "screeplots", "scree"))
         
         else:
@@ -206,9 +210,10 @@ class basePCA(TransformerMixin, BaseEstimator):
             os.makedirs(os.path.join(self.path,"screeplots"),exist_ok=True)
 
             save_wordclouds(self.loadings,os.path.join(self.path,f"wordclouds_{group}"))
-            self.project_columns.to_csv(os.path.join(self.path,f"csvdata_{group}","pca_scores.csv"))
+            self.project_columns.to_csv(os.path.join(self.path,f"csvdata_{group}","projected_pca_scores.csv"))
+            self.extra_columns.to_csv(os.path.join(self.path, "csvdata","fitted_pca_scores.csv"))
             self.loadings.to_csv(os.path.join(self.path,f"csvdata_{group}","pca_loadings.csv"))
-            #pd.concat([self.ogdf, self.check_inputs(self.project_columns)], axis=1).to_csv(os.path.join(self.path,f"csvdata_{group}","pca_scores_original_format.csv"))
+            pd.concat([self.ogdf, self.check_inputs(self.extra_columns)], axis=1).to_csv(os.path.join(self.path,f"csvdata_{group}","pca_scores_original_format.csv"))
             plot_scree(self.fullpca,os.path.join(self.path, "screeplots", f"scree_{group}"))
         
         print(f"Saving done. Results have been saved to {self.path}")
